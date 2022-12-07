a114fcfeab2333fc5eef9489f58d6fd57c126f8b
Melvin Welton <nivlem65@gmail.com>
2022-12-07 13:50:18 -0600

    fetch

--- module-6-starter 4/assets/js/homepage.js
+++ module-6-starter 4/assets/js/homepage.js
@@ -1,8 +1,12 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function () {
+    console.log("Is this working?");
+    var response = fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+        console.log("inside", response);
+        console.log("outside", response);
+        // Notice that the data is coming back as undefined
+        // This is because the fetch() method is asyn
 
-  };
-  
-  getUserRepos();
-  
+    });
+
+};
+    getUserRepos();
